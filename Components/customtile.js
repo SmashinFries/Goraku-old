e2383537b5c82b123385397afbf3503e6f218e7c
SmashinFries <smashinfries1@gmail.com>
2021-09-06 16:02:49 -0500

    updated styling

--- Components/customtile.js
+++ Components/customtile.js
@@ -28,13 +28,13 @@ export const _ContentTile = ({ item, routeName }) => {
                 imageProps={{style: {resizeMode:'cover', borderRadius: 8}}}
                 title={(lang !== 'Native') ? item.title.romaji : item.title.native}
                 titleNumberOfLines={2}
-                titleStyle={{ color:colors.text, fontSize: 15, position: 'absolute', top: 3 }}
+                titleStyle={{ color:colors.text, fontSize: 15, position: 'absolute', top: 3, textAlign:'center', alignSelf:'center' }}
                 containerStyle={{borderRadius: 8, backgroundColor: 'rgba(0,0,0,0)'}}
                 activeOpacity={0.7}
                 width={width / 2.5}
                 height={height / 3}
                 key={item.id}
-                onPress={() => {navigation.push((routeName.name === 'SearchPage') ? 'InfoSearch' : 'InfoHome', {screen: 'Info', params: {id:item.id, title:{romaji: item.title.romaji, native: item.title.native}},});}}
+                onPress={() => {navigation.push((routeName.name === 'SearchPage') ? 'InfoSearch' : 'InfoHome', {screen: 'Info', params: {id:item.id, title:{romaji: item.title.romaji, native: item.title.native, english:item.title.english}},});}}
                 />
             <Badge value={(typeof item.meanScore == 'number') ? `${item.meanScore}%` : '?'}
                 containerStyle={{ alignSelf: 'flex-end', position: 'absolute', elevation: 24, top:-5 }}
