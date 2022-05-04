a971126eedc2e2570bd58ff19546f325fd2e3a76
SmashinFries <smashinfries1@gmail.com>
2022-05-04 16:36:05 -0500

    add github fetches

--- src/Api/github/github.ts
+++ src/Api/github/github.ts
@@ -1,9 +1,30 @@
 import axios from "axios";
 import { useEffect, useState } from "react";
 import { VERSION } from "../../constants";
-import { Release } from "./types";
+import { Current, Release } from "./types";
 
-const RELEASE_URL = 'https://api.github.com/repos/smashinfries/goraku/releases';
+const RELEASE_URL = 'https://api.github.com/repos/smashinfries/goraku/releases/latest';
+const CURRENT_URL = 'https://api.github.com/repos/smashinfries/goraku/releases/tags/';
+
+export const fetchRelease = async () => {
+    try {
+        const res = await axios.get<Release>(RELEASE_URL);
+        return res.data;
+    } catch (e) {
+        console.warn('Release:', e);
+        return null;
+    }
+}
+
+export const fetchCurrent = async (version:string) => {
+    try {
+        const res = await axios.get<Current>(CURRENT_URL+version);
+        return res.data;
+    } catch (e) {
+        console.warn('Current:', e);
+        return null;
+    }
+}
 
 export const useRelease = () => {
     const [release, setRelease] = useState<Release>();
