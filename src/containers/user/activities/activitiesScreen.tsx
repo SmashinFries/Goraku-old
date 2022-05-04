a5193b436de09843fe151418f300850287ba2bd6
SmashinFries <smashinfries1@gmail.com>
2022-05-04 16:59:04 -0500

    removed unused function

--- src/containers/user/activities/activitiesScreen.tsx
+++ src/containers/user/activities/activitiesScreen.tsx
@@ -7,25 +7,6 @@ import { UserStackProps } from "../../types";
 import ActivityTile from "./components/activityTile";
 import { useHeaderHeight } from "@react-navigation/elements";
 
-// const DropDownExtra = () => {
-//     return(
-//         <SelectDropdown
-//                         data={['Following', 'Personal']}
-//                         buttonTextAfterSelection={(selected, index) => {return selected}}
-//                         rowTextForSelection={(item, index) => {return item}}
-//                         defaultValue={showFollowing ? 'Following' : 'Personal'}
-//                         defaultButtonText={showFollowing ? 'Following' : 'Personal'}
-//                         onSelect={(selected) => setShowFollowing(selected === 'Following' ?  true : false)}
-//                         dropdownStyle={{backgroundColor:colors.card, borderRadius:12}}
-//                         rowStyle={{backgroundColor:colors.card, borderBottomWidth:0}}
-//                         buttonStyle={{backgroundColor:colors.background, borderRadius:12, height:40, width:180}}
-//                         renderDropdownIcon={() => <AntDesign name="down" size={16} color={colors.primary} style={{ marginRight: 10 }} />}
-//                         rowTextStyle={{textTransform:'capitalize', color:colors.text}}
-//                         buttonTextStyle={{textTransform:'capitalize', color:colors.text}}
-//                     />
-//     );
-// }
-
 const ActivitiesScreen = ({navigation, route}:UserStackProps) => {
     const [page, setPage] = useState(1);
     const {data, setData, loading, refresh, onRefresh, userId} = useActivities(page);
@@ -54,13 +35,13 @@ const ActivitiesScreen = ({navigation, route}:UserStackProps) => {
 
     return (
         <FlatList 
-            data={data.activities ?? []}
+            data={data?.activities ?? []}
             renderItem={({item}) => <ActivityTile item={item} userId={Number(userId)} colors={colors} deleteActivity={deleteActivity} navigation={navigation} />}
             contentContainerStyle={{paddingHorizontal:15, paddingVertical:headerHeight+30}}
             ItemSeparatorComponent={() => <View style={{height:30}} />}
             keyExtractor={({id}) => id.toString()}
             refreshing={refresh}
-            onRefresh={() => onRefresh()}
+            onRefresh={onRefresh}
             onEndReached={(data?.pageInfo.hasNextPage) && loadMore}
             onEndReachedThreshold={0.4}
         />
