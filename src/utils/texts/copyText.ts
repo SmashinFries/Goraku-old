0bd2749cf8d78a68b9d14c8c26e16c3d6b0b49e2
SmashinFries <smashinfries1@gmail.com>
2022-05-04 17:01:53 -0500

    add toast

--- src/utils/texts/copyText.ts
+++ src/utils/texts/copyText.ts
@@ -1,8 +1,10 @@
 import React from "react";
 import * as Clipboard from 'expo-clipboard';
 import * as Haptics from 'expo-haptics';
+import { ToastAndroid } from "react-native";
 
 export const handleCopy = (text:string) => {
     Haptics.impactAsync().catch((err) => console.log(err));
     Clipboard.setString(text);
+    ToastAndroid.show(`Copied to clipboard:\n${text}`, ToastAndroid.SHORT);
 }
