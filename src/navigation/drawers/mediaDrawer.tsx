6d424e29d5abc89dc8160c7032a0b4ed79f09a59
SmashinFries <smashinfries1@gmail.com>
2022-05-04 17:07:28 -0500

    fixed drawer bug

--- src/navigation/drawers/mediaDrawer.tsx
+++ src/navigation/drawers/mediaDrawer.tsx
@@ -3,7 +3,7 @@ import React, { useEffect, useState } from "react";
 import { View, Text } from "react-native";
 import { getMediaInfo, getMalData } from "../../Api";
 import { AniMalType } from "../../Api/types";
-import { createDrawerNavigator, DrawerContentScrollView, DrawerItem, DrawerItemList } from "@react-navigation/drawer";
+import { createDrawerNavigator } from "@react-navigation/drawer";
 import { InfoProps } from "../../containers/types";
 import { createNativeStackNavigator } from "@react-navigation/native-stack";
 import { getMalImages } from "../../Api/mal";
@@ -64,7 +64,7 @@ export const InfoDrawer = ({ navigation, route }:InfoProps) => {
     if (loading) return <LoadingView titleData={[{title:'Anilist Data', loading:aniLoading}, {title:'MAL Data', loading:malLoading}, {title:'MAL Images', loading:imageLoading}]} colors={{colors, dark}} />;
     if (data === null) return <View style={{ flex: 1, justifyContent: 'center', alignItems: 'center' }}><Text style={{color:colors.text, fontSize:18}}>Error</Text></View>
     return (
-        <Drawer.Navigator initialRouteName="Overview" backBehavior="firstRoute" screenOptions={({ navigation, route }) => ({
+        <Drawer.Navigator useLegacyImplementation initialRouteName="Overview" backBehavior="firstRoute" screenOptions={({ navigation, route }) => ({
             drawerPosition: 'right',
             drawerType: 'back',
             headerTransparent: true,
@@ -78,18 +78,19 @@ export const InfoDrawer = ({ navigation, route }:InfoProps) => {
             {(auth) && <Drawer.Screen name="Following" component={FollowingTab} initialParams={{id:data.anilist.id}} options={{headerShown:true, headerTransparent:false, headerTitle:'Following', headerStyle:{elevation:8}}}/>}
             {(data.anilist.type === 'ANIME') ? <Drawer.Screen name="Music" component={MusicTab} initialParams={{ id, coverImage: data.anilist.coverImage.extraLarge }} /> : null}
             {(data.mal) && <Drawer.Screen name="News" component={NewsTab} initialParams={{ mal_id:data.anilist.idMal, coverImage: data.anilist.coverImage.extraLarge, type: data.anilist.type }} />}
-            {(data.anilist.studios.nodes.length > 0) ? <Drawer.Screen name="StudioInfo" component={StudioInfo} initialParams={{id: data.anilist.studios.nodes[0].id, name: data.anilist.studios.nodes[0].name}} options={{headerTitle:`${data.anilist.studios.nodes[0].name}`, drawerLabel:`${data.anilist.studios.nodes[0].name}`, headerStyle:{backgroundColor:colors.card}, headerTransparent:false}} /> : null}
+            {(data.anilist.studios.nodes.length > 0) ? <Drawer.Screen name="StudioInfo" component={StudioInfo} initialParams={{id: data.anilist.studios.nodes[0].id, name: data.anilist.studios.nodes[0].name}} options={{headerTitle:`${data.anilist.studios.nodes[0].name}`, drawerLabel:`Studio`, headerStyle:{backgroundColor:colors.card}, headerTransparent:false}} /> : null}
         </Drawer.Navigator>
     );
 }
 
 export const DrawerStack = ({navigation, route}) => {
     const id = route.params.id;
+    const isList = route.params.isList;
     return(
         <Stack.Navigator >
             <Stack.Screen name="DrawerInfo" component={InfoDrawer} options={{headerShown:false}} initialParams={{id: id}} />
             <Stack.Screen name='Character' component={CharDetailScreen} options={{ headerTransparent:false}} />
-            <Stack.Screen name="Staff" component={StaffInfo} options={{ headerTransparent:false}} />
+            <Stack.Screen name="Staff" component={StaffInfo} options={{ headerTransparent:false}} initialParams={{isList:isList}} />
         </Stack.Navigator>
     );
 }
