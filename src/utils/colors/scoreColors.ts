a0141f6c952e4e4295e036a600cc6d6e6634c8a7
SmashinFries <smashinfries1@gmail.com>
2022-05-04 18:51:17 -0500

    remove dumb linkColor function

--- src/utils/colors/scoreColors.ts
+++ src/utils/colors/scoreColors.ts
@@ -29,65 +29,6 @@ export const getMalScoreColor = (score:number):string => {
     }
 }
 
-export const getExternalLinkColor = (site:string):string => {
-    const { colors } = useTheme();
-    const LINKS = {
-        'Official Site': colors.primary,
-        'Amazon': '#FF9900',
-        'Animax': '#00A3FB',
-        'Animenetwork': '#9880FA',
-        'Asian Crush': '#FD5C25',
-        'Bomtoon': '#821BE6',
-        'Comico': '#F40000',
-        // 'ComicWalker'
-        // 'CONtv'
-        'Crunchyroll': '#F47520',
-        // 'Dajiaochong Manhua'
-        // 'Daum Webtoon'
-        'Facebook':'#4267B2',
-        // 'Fakku'
-        'Funimation':'#5B0BB5',
-        'HBO Max': '#7120D9',
-        'Hidive': '#00AEEF',
-        'Hulu': '#1CE783',
-        // 'Japanese Film Archives'
-        // 'Justoon'
-        // 'KakaoPage'
-        // 'KuaiKan Manhua'
-        // 'Lezhin'
-        // 'Lezhin'
-        // 'Madman'
-        'Manga Plus': '#E7240B',
-        // 'Manga.Club'
-        // 'Mangabox'
-        // 'Manman Manhua'
-        'Midnight Pulp': '#00D640',
-        // 'Naver'
-        'Netflix': '#E50914',
-        // 'Nico Nico Seiga'
-        // 'Piccoma'
-        // 'Pixiv Comic'
-        // 'Pixiv Novel'
-        // 'Pocket Magazine'
-        // 'QQ'
-        'Shonen Jump Plus': '#E53935',
-        // 'Sony Crackle'
-        // 'Toomics'
-        // 'Toomics'
-        // 'Tubi TV'
-        'Twitter': '#1D9BF0',
-        // 'Viewster'
-        'Vimeo': '#86c9ef',
-        'Viz': '#FF0000',
-        'VRV': '#FFDD00',
-        // 'Web Comics'
-        'Webtoons': '#00DC64',
-        // 'Weibo Manhua'
-        'Youtube': '#FF0000'
-    }
-    return LINKS[site];
-}
-
 export const listColor = (status: string) => {
     switch(status) {
         case 'COMPLETED':
