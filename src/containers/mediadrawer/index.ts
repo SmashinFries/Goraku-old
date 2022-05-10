08dc88838389718ed83e07c6c8b1e752a9ca009a
SmashinFries <smashinfries1@gmail.com>
2022-05-09 20:37:46 -0500

    added review components

--- src/containers/mediadrawer/index.ts
+++ src/containers/mediadrawer/index.ts
@@ -3,5 +3,7 @@ import StudioInfo from "./studio/studio";
 import MediaInfoScreen from "./overview/overview";
 import MusicTab from "./music/music";
 import NewsTab from "./news/news";
+import ReviewsTab from "./reviews/reviews";
+import ReviewBody from "./reviews/reviewBody";
 
-export { StudioInfo, WatchTab, MediaInfoScreen, MusicTab, NewsTab };
+export { StudioInfo, WatchTab, MediaInfoScreen, MusicTab, NewsTab, ReviewsTab, ReviewBody };
