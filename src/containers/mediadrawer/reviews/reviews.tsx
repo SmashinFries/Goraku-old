e6a54c92bace336971753cfe5bd6a13c6f8e9fe7
SmashinFries <smashinfries1@gmail.com>
2022-05-12 20:40:36 -0500

    added reviews page

---
+++ src/containers/mediadrawer/reviews/reviews.tsx
@@ -0,0 +1,30 @@
+import { useTheme } from "@react-navigation/native";
+import React, { useState } from "react";
+import { View, Text, FlatList } from "react-native";
+import { Reviews, ReviewsNode } from "../../../Api/types";
+import { ReviewProps } from "../../types";
+import { SummaryCard } from "./components/summaryCard";
+
+const ReviewsTab = ({navigation, route}:ReviewProps) => {
+    const [data, setData] = useState<ReviewsNode[]>(route.params.reviews);
+    const { colors } = useTheme();
+    
+    const renderItem = ({item}:{item:ReviewsNode}) => {
+        return(
+            // @ts-ignore
+            <SummaryCard colors={colors} review={item} onPress={() => navigation.navigate('ReviewBody', {review: item, setData:setData})} />
+        );
+    }
+    return(
+        <View style={{flex:1}}>
+            <FlatList 
+                data={data}
+                renderItem={renderItem}
+                keyExtractor={(item) => item.node.id.toString()}
+                ItemSeparatorComponent={() => <View style={{height:10}}/>}
+            />
+        </View>
+    );
+}
+
+export default ReviewsTab;
