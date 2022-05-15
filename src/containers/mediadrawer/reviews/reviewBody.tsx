f420e0b5901980cb3d807526b0c084a7a66c70d8
SmashinFries <smashinfries1@gmail.com>
2022-05-14 20:41:32 -0500

    created review body component

---
+++ src/containers/mediadrawer/reviews/reviewBody.tsx
@@ -0,0 +1,51 @@
+import { useTheme } from "@react-navigation/native";
+import { openURL } from "expo-linking";
+import React, { useEffect, useLayoutEffect, useState } from "react";
+import { useWindowDimensions, Pressable } from "react-native";
+import { ScrollView } from "react-native-gesture-handler";
+import { Avatar } from "react-native-paper";
+import RenderHtml from 'react-native-render-html';
+import { getReviewUserRating, rateReview } from "../../../Api/anilist/anilist";
+import { ReviewsNode, userRatingReview } from "../../../Api/types";
+import { UserModal } from "../../../Components";
+import { FinalScore, RateReview } from "./components/reviewBottom";
+import { customHTMLElementModel } from "./elements/customElements";
+
+const ReviewBody = ({ navigation, route }) => {
+    const review:ReviewsNode = route.params.review;
+    const [visible, setVisible] = useState(false);
+    const [userRating, setUserRating] = useState<userRatingReview>();
+    const { colors, dark } = useTheme();
+    const { width } = useWindowDimensions();
+
+    useEffect(() => {
+        const fetchUserRating = async() => {
+            const newRating = await getReviewUserRating(review.node.id);
+            console.log('Fetched!', newRating);
+            setUserRating(newRating);
+        }
+        fetchUserRating();
+    },[]);
+
+    useLayoutEffect(() => {
+        navigation.setOptions({
+            title: review.node.user.name + "'s Review",
+            headerRight: () => <Pressable onPress={() => {setVisible(true)}}><Avatar.Image source={{uri:review.node.user.avatar.large}} size={42 } style={{backgroundColor:review.node.user.options.profileColor}} /></Pressable>
+        });
+    },[navigation, dark]);
+
+    return(
+            <ScrollView contentContainerStyle={{paddingHorizontal:10}}>
+                <RenderHtml
+                    contentWidth={width}
+                    source={{html:review.node.body.replace(/[\[\]']+/g,'').replace(/\(\)+/g,'')}}
+                    customHTMLElementModels={customHTMLElementModel}
+                />
+                <FinalScore score={review.node.score} />
+                {(userRating) && <RateReview action={(vote:userRatingReview) => {rateReview(review.node.id, vote)}} userRating={userRating} rating={review.node.rating} totalRatings={review.node.ratingAmount} colors={colors} />}
+                <UserModal user={review.node.user} visible={visible} onDismiss={() => setVisible(false)} colors={colors} />
+            </ScrollView>
+    );
+}
+
+export default ReviewBody;
