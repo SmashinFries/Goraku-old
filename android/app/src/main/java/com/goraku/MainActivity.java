e7456e0c37d56d1e5834f46f72fc01dd3f9f49d0
SmashinFries <smashinfries1@gmail.com>
2022-05-24 03:39:40 -0500

    update

--- android/app/src/main/java/com/goraku/MainActivity.java
+++ android/app/src/main/java/com/goraku/MainActivity.java
@@ -19,6 +19,12 @@ public class MainActivity extends ReactActivity {
     super.onCreate(null);
   }
 
+  @Override
+  protected void onStart() {
+    super.onStart();
+    RNInAppBrowserModule.onStart(this);
+  }
+
   /**
    * Returns the name of the main component registered from JavaScript.
    * This is used to schedule rendering of the component.
