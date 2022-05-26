29c427b4371f2e7f8a02cc2b51af64ad93425fb8
SmashinFries <smashinfries1@gmail.com>
2022-05-25 23:53:36 -0500

    use SiteButton component

--- src/containers/mediadrawer/overview/components/links.tsx
+++ src/containers/mediadrawer/overview/components/links.tsx
@@ -2,6 +2,7 @@ import React, { useEffect } from "react";
 import { View, Text, Linking, Pressable, ViewStyle } from "react-native";
 import FastImage from "react-native-fast-image";
 import { AniMalType, ExtLink } from "../../../../Api/types";
+import { SiteButton } from "../../../../Components/buttons/siteButtons";
 import { AnilistSVG, MalSVG } from "../../../../Components/svg/svgs";
 import { ThemeColors } from "../../../../Components/types";
 import { _openBrowserUrl } from "../../../../utils";
@@ -10,6 +11,7 @@ const ExternalLinkList = ({data, colors}:{data:AniMalType, colors:ThemeColors}) 
     const officialLinks = data.anilist.externalLinks.filter((link, index) => link.site === 'Official Site');
     const malLink = data.mal?.data?.url;
     const openLink = (link:string) => _openBrowserUrl(link, colors.primary, colors.text)
+    const uniWidth = 85;
 
     const LinkButton = ({link, width, height}:{link:ExtLink, width?:number|string, height?:number|string}) => {
         // () => Linking.openURL(link.url)
@@ -34,20 +36,16 @@ const ExternalLinkList = ({data, colors}:{data:AniMalType, colors:ThemeColors}) 
                 </View>
             )}
             
-            <View style={{flex:1, flexDirection: 'row', justifyContent: 'center', paddingLeft:5, flexWrap: 'wrap', alignItems: 'center'}}>
+            <View style={{flex:1, flexDirection: 'row', justifyContent: 'space-between', paddingHorizontal:5, flexWrap: 'wrap', width:'100%', alignItems: 'center'}}>
                 <View style={{ marginVertical: 5, marginHorizontal: 5, borderRadius:8 }}>
-                    <Pressable onPress={() => openLink(data.anilist.siteUrl)} style={{ height: 50, width: 85, alignItems:'center', borderRadius:8, justifyContent:'center', backgroundColor: 'rgb(60,180,242)'}}>
-                        <AnilistSVG color={'#FFF'} width={35} height={35} />
-                    </Pressable>
+                    <SiteButton type="anilist" link={data.anilist.siteUrl} colors={colors} width={uniWidth} />
                 </View>
                 {data.anilist.externalLinks.filter((offlink, index) => offlink.site !== 'Official Site').map((link, index) =>
-                    <LinkButton key={index} link={link} width={85} height={50} />
+                    <LinkButton key={index} link={link} width={uniWidth} height={50} />
                 )}
                 {malLink && 
                     <View style={[{ marginVertical: 5, borderRadius:8, marginHorizontal: 5, }]}>
-                        <Pressable onPress={() => openLink(malLink)} style={{ height: 50, width:85, alignItems:'center', justifyContent:'center', borderRadius:8, backgroundColor: '#2A50A3'}}>
-                            <MalSVG color='#FFF' width={45} height={45} />
-                        </Pressable>
+                        <SiteButton type='mal' link={malLink} colors={colors} width={uniWidth} />
                     </View>
                 }
             </View>
