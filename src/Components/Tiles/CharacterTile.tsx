768050a28c27ecae8a12d3ed9b750ad5b08b5017
SmashinFries <smashinfries1@gmail.com>
2022-05-28 19:57:38 -0500

    use new date checker

--- src/Components/Tiles/CharacterTile.tsx
+++ src/Components/Tiles/CharacterTile.tsx
@@ -3,10 +3,11 @@ import { Pressable, View, Text } from "react-native";
 import FastImage from "react-native-fast-image";
 import { IconButton } from "react-native-paper";
 import { CharacterSearchTile } from "../../Api/types";
+import { checkBD } from "../../utils";
 
 type CharacterTile = {
     character: CharacterSearchTile;
-    date: {day:number, month:number};
+    date: Date;
     primaryColor: string;
     onPress: () => void;
 }
@@ -16,7 +17,7 @@ export const CharacterTile = ({character, date, onPress, primaryColor}:Character
             <FastImage fallback source={{uri: character.image.large}} style={{height:210, width:130, borderRadius:8}}  resizeMode='cover' />
             <LinearGradient colors={['transparent', (character.isFavourite) ? 'rgba(255, 0, 0,.85)' : 'rgba(0,0,0,.8)']} locations={[.75, 1]} style={{position:'absolute', borderRadius:8, justifyContent:'flex-end', alignItems:'center', width:'100%', height:'100%'}}>
                 <Text style={{color:'#FFF', paddingBottom:5}} numberOfLines={2}>{character.name.userPreferred}</Text>
-                {(date.day+1 === character.dateOfBirth.day && date.month === character.dateOfBirth.month) && <IconButton icon={'cake-variant'} color={primaryColor} style={{position:'absolute', top:-10, right:-5}} />}
+                {(checkBD(date, character.dateOfBirth)) && <IconButton icon={'cake-variant'} color={primaryColor} style={{position:'absolute', top:-10, right:-5}} />}
             </LinearGradient>
             <Pressable onPress={onPress} style={{position:'absolute', height:'100%', width:'100%', borderRadius:8}} android_ripple={{color:primaryColor}} />
         </View>
