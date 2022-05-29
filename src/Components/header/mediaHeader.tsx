da8b4574ad6a573e811bdc990af6158435304cd4
SmashinFries <smashinfries1@gmail.com>
2022-05-28 19:39:09 -0500

    use fastimage

--- src/Components/header/mediaHeader.tsx
+++ src/Components/header/mediaHeader.tsx
@@ -2,6 +2,7 @@ import { useTheme } from "@react-navigation/native";
 import { LinearGradient } from "expo-linear-gradient";
 import React from "react";
 import { View, Image, Text, useWindowDimensions } from "react-native";
+import FastImage from "react-native-fast-image";
 
 export const MediaHeader = ({coverImage, loc=[0, .95]}) => {
     
@@ -10,7 +11,7 @@ export const MediaHeader = ({coverImage, loc=[0, .95]}) => {
     return (
         <View style={{ position:'absolute', width:'100%', backgroundColor: 'transparent'}}>
             <View>
-                <Image source={{ uri: coverImage }} style={{ position: 'absolute', zIndex: -1, height: height, width: '100%', resizeMode: 'cover' }} />
+                <FastImage fallback source={{ uri: coverImage }} style={{ position: 'absolute', zIndex: -1, height: height, width: '100%' }} resizeMode='cover'/>
                 <LinearGradient colors={(!dark) ? ['rgba(242, 242, 242, .4)', colors.background] : ['rgba(0, 0, 0, .4)', colors.background]} locations={loc} style={{ position: 'absolute', height: height, width: '100%' }} />
             </View>
         </View>
