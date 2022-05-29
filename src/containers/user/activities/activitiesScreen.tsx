085784763267cbe76493c6343a8565b8469821fe
SmashinFries <smashinfries1@gmail.com>
2022-05-28 19:49:39 -0500

    fixed load more

--- src/containers/user/activities/activitiesScreen.tsx
+++ src/containers/user/activities/activitiesScreen.tsx
@@ -1,16 +1,20 @@
 import { useTheme } from "@react-navigation/native";
 import React, { useEffect, useState } from "react";
 import { View, FlatList } from "react-native";
-import { removeActivity, useActivities } from "../../../Api/anilist/anilist";
+import { fetchActivity, removeActivity } from "../../../Api/anilist/anilist";
 import { LoadingView } from "../../../Components";
 import { UserStackProps } from "../../types";
 import ActivityTile from "./components/activityTile";
 import { useHeaderHeight } from "@react-navigation/elements";
+import { ActivityPage } from "../../../Api/types";
+import { useUserId } from "../../../Storage/authToken";
 
 const ActivitiesScreen = ({navigation, route}:UserStackProps) => {
-    const [page, setPage] = useState(1);
-    const {data, setData, loading, refresh, onRefresh, userId} = useActivities(page);
+    const [data, setData] = useState<ActivityPage>();
+    const [loading, setLoading] = useState<boolean>(true);
+    const [refresh, setRefresh] = useState<boolean>(false);
     const { colors, dark } = useTheme();
+    const userId = useUserId();
     const headerHeight = useHeaderHeight();
 
     const deleteActivity = async(id:number) => {
@@ -20,10 +24,18 @@ const ActivitiesScreen = ({navigation, route}:UserStackProps) => {
     }
 
     const loadMore = async() => {
-        if (data.pageInfo.hasNextPage){
-            setPage(page + 1);
+        if (data.pageInfo.hasNextPage) {
+            const response = await fetchActivity(data.pageInfo.currentPage + 1);
+            setData({...data, activities: [...data.activities, ...response.activities], pageInfo: response.pageInfo});
         }
     }
+
+    const onRefresh = async() => {
+        setRefresh(true);
+        const resp = await fetchActivity();
+        setData(resp);
+        setRefresh(false);
+    } 
 
     useEffect(() => {
         navigation.setOptions({
@@ -31,12 +43,25 @@ const ActivitiesScreen = ({navigation, route}:UserStackProps) => {
         });
     },[dark, navigation]);
 
+    useEffect(() => {
+        fetchActivity().then(resp => {
+            setData(resp);
+            setLoading(false);
+        });
+    },[])
+
+    const renderItem = ({ item }) => {
+        return(
+            <ActivityTile item={item} userId={Number(userId)} colors={colors} deleteActivity={deleteActivity} navigation={navigation} />
+        );
+    }
+
     if (loading) return <LoadingView colors={{colors, dark}} />
 
     return (
         <FlatList 
             data={data?.activities ?? []}
-            renderItem={({item}) => <ActivityTile item={item} userId={Number(userId)} colors={colors} deleteActivity={deleteActivity} navigation={navigation} />}
+            renderItem={renderItem}
             contentContainerStyle={{paddingHorizontal:15, paddingVertical:headerHeight+30}}
             ItemSeparatorComponent={() => <View style={{height:30}} />}
             keyExtractor={({id}) => id.toString()}
