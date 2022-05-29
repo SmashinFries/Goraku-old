65e63f224cb853db07334a7214c9560748cf4854
SmashinFries <smashinfries1@gmail.com>
2022-05-28 19:46:25 -0500

    added favorites view

--- src/Components/dialogs/userModal.tsx
+++ src/Components/dialogs/userModal.tsx
@@ -3,10 +3,51 @@ import { StyleSheet, TextStyle, useWindowDimensions, View, ViewStyle } from "rea
 import { Button, Modal, Portal, Text } from "react-native-paper";
 import { ThemeColors } from "../types";
 import FastImage, { ImageStyle } from "react-native-fast-image";
-import { FollowerListUser, UserBasic } from "../../Api/types";
+import { FollowerListUser, UserBasic, UserFavChar, UserFavMed, UserFavorites } from "../../Api/types";
 import { openURL } from "expo-linking";
 import { convertUnixTime } from "../../utils/time/getTime";
-import { _openBrowserUrl } from "../../utils"
+import { handleCopy, _openBrowserUrl } from "../../utils"
+
+type FavViewProps = {
+    favourites: UserFavorites;
+    colors: ThemeColors;
+}
+const FavoritesView = ({favourites, colors}:FavViewProps) => {
+    const anime = favourites.anime.nodes[0] ?? null;
+    const manga = favourites.manga.nodes[0] ?? null;
+    const character = favourites.characters?.nodes[0] ?? null;
+    const gender = favourites.characters?.nodes[0]?.gender ?? 'Female'
+
+    const ImageItem = ({image, title, type}:{image:string, title:string, type:string}) => {
+        return(
+            <View style={{flex:1, alignItems:'center'}}>
+                <Text style={{
+                    color:colors.text,
+                    fontWeight:'bold',
+                    fontSize:18,
+                    textShadowColor: colors.primary,
+                    textShadowOffset: { width: 0.5, height: 0.5 },
+                    textShadowRadius: 2
+                    }}>
+                        {type}
+                </Text>
+                <FastImage fallback source={{uri:image}} style={{height:120, width:80}} resizeMode='cover' />
+                <Text onPress={() => handleCopy(title)} style={{textAlign:'center', color:colors.text}} numberOfLines={3}>{title}</Text>
+            </View>
+        );
+    }
+
+    return(
+        <View style={{marginTop:20}}>
+            <Text style={{color:colors.text, fontSize:20, textDecorationLine:'underline', fontWeight:'bold', textAlign:'center'}}>Favorites</Text>
+            <View style={{flexDirection:'row', justifyContent:'space-evenly'}}>
+                {(anime) && <ImageItem type={'Anime'} image={anime.coverImage.extraLarge} title={anime.title.userPreferred} />}
+                {(manga) && <ImageItem type={'Manga'} image={manga.coverImage.extraLarge} title={manga.title.userPreferred} />}
+                {(character) && <ImageItem type={(gender === 'Male') ? 'Husbando' : 'Waifu'} image={character.image.large} title={character.name.userPreferred} />}
+            </View>
+        </View>
+    );
+}
 
 type Props = {
     user: UserBasic | FollowerListUser;
@@ -14,7 +55,7 @@ type Props = {
     onDismiss: () => void;
     colors: ThemeColors;
 }
-type Styles = {
+type Styles = { 
     container: ViewStyle;
     avatarContainer: ViewStyle;
     bannerContainer: ViewStyle;
@@ -90,6 +131,7 @@ const UserModal = ({user, visible, onDismiss, colors}:Props) => {
                     </View>
                     <Text style={modalStyle.userName}>{user.name}</Text>
                     <Text style={modalStyle.createdText}>Created {convertUnixTime(user.createdAt)}</Text>
+                    <FavoritesView favourites={user.favourites} colors={colors} />
                     <Button mode="outlined" color={colors.primary} onPress={() => _openBrowserUrl(user.siteUrl, colors.primary, colors.text)} style={modalStyle.viewButton}>View Profile</Button>
                 </View>
             </Modal>
