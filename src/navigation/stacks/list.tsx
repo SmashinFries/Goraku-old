e55a01146512778ef70e68ee45cb115651914597
SmashinFries <smashinfries1@gmail.com>
2022-06-08 02:32:48 -0500

    seperated favorites from userlist

--- src/navigation/stacks/list.tsx
+++ src/navigation/stacks/list.tsx
@@ -6,6 +6,7 @@ import { ListTabs } from "../tabs/listTabs";
 import { CharDetailScreen } from "../../containers/character";
 import { View } from "react-native";
 import { ListSearchProvider } from "../../Storage/listStorage";
+import FavTabs from "../tabs/favoritesTab";
 
 const Stack = createNativeStackNavigator();
 
@@ -14,7 +15,8 @@ export const ListStack = ({navigation}) => {
         <View style={{flex:1}} collapsable={false}>
             <ListSearchProvider>
                 <Stack.Navigator initialRouteName="UserList" screenOptions={{headerShown:false}}>
-                    <Stack.Screen name="UserList" component={ListTabs} options={{headerShown:true,}} initialParams={{type: 'ANIME', format:'Any', layout:'column'}} />
+                    <Stack.Screen name="UserList" component={ListTabs} options={{headerShown:true,}} initialParams={{format:'Any', type:'ANIME'}} />
+                    <Stack.Screen name='Favorites' component={FavTabs} options={{headerShown:true,}} />
                     <Stack.Screen name="UserListDetail" component={DrawerStack} initialParams={{isList:true}} />
                     <Stack.Screen name='UserCharDetail' component={CharDetailScreen} options={{ headerShown:true, headerTintColor:'#FFF' }} />
                     <Stack.Screen name='UserStaffDetail' component={StaffInfo} options={{ headerShown:true, headerTransparent:true }} />
