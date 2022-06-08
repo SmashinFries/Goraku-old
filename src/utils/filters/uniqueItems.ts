8e20807bf17c87315dd16bd0252c513132ba49ff
SmashinFries <smashinfries1@gmail.com>
2022-06-08 02:24:26 -0500

    create unique nodes function

--- src/utils/filters/uniqueItems.ts
+++ src/utils/filters/uniqueItems.ts
@@ -1,10 +1,22 @@
-import { MediaTileType } from "../../Api/types";
+import { MediaTileType, UserFavCharNode } from "../../Api/types";
 
 export const uniqueItems = (content: MediaTileType[]) => {
     const setObj = new Set();
     const result = content.reduce((acc, item) => {
         if (!setObj.has(item.id)) {
             setObj.add(item.id);
+            acc.push(item);
+        }
+        return acc;
+    }, []);
+    return result;
+}
+
+export const uniqueNodes = (content: UserFavCharNode[] | any) => {
+    const setObj = new Set();
+    const result = content.reduce((acc, item) => {
+        if (!setObj.has(item.node.id)) {
+            setObj.add(item.node.id);
             acc.push(item);
         }
         return acc;
