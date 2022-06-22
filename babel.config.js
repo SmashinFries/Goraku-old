c1ab1d09015a3d6b1a8b6295a2eb14d6cd208097
SmashinFries <smashinfries1@gmail.com>
2022-06-21 23:10:38 -0500

    update

--- babel.config.js
+++ babel.config.js
@@ -2,6 +2,10 @@ module.exports = function(api) {
   api.cache(true);
   return {
     presets: ['babel-preset-expo'],
-    plugins: ['react-native-paper/babel', 'react-native-reanimated/plugin']
+    plugins: [
+      'react-native-paper/babel',
+      'react-native-reanimated/plugin',
+      ['module:react-native-dotenv']
+    ]
   };
 };
