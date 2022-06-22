cf4860266e6a01039042e67f0f5e22f4f42b0392
SmashinFries <smashinfries1@gmail.com>
2022-06-21 23:27:16 -0500

    added anilist account screen

--- src/navigation/stacks/account.tsx
+++ src/navigation/stacks/account.tsx
@@ -1,6 +1,7 @@
 import { useTheme } from "@react-navigation/native";
 import { createNativeStackNavigator } from "@react-navigation/native-stack";
 import { AccountHome, ChooseLanguage } from "../../containers/more/account/account";
+import AnilistAccount from "../../containers/more/account/anilistaccount";
 
 const Stack = createNativeStackNavigator();
 
@@ -9,6 +10,7 @@ const AccountStack = () => {
     return(
         <Stack.Navigator initialRouteName="AccountHome" screenOptions={{headerStyle:{backgroundColor:colors.card}}}>
             <Stack.Screen name="AccountHome" component={AccountHome} initialParams={{token:''}} options={{title:'Account'}} />
+            <Stack.Screen name="AnilistAccount" component={AnilistAccount} options={{title:'Anilist'}} />
             <Stack.Screen name="Languages" component={ChooseLanguage} options={{title:'Account'}} />
         </Stack.Navigator>
     );
