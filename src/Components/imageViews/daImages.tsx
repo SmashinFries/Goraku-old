71834f0997065e5a9a7fa84b63c6d1a56fd122fb
SmashinFries <smashinfries1@gmail.com>
2022-06-25 05:38:52 -0500

    added view more buttons

--- src/Components/imageViews/daImages.tsx
+++ src/Components/imageViews/daImages.tsx
@@ -1,24 +1,29 @@
+import { NavigationProp } from "@react-navigation/native";
 import React, { useEffect, useState } from "react";
 import { FlatList, Pressable, View, Text } from "react-native";
 import FastImage from "react-native-fast-image";
+import { Button, IconButton } from "react-native-paper";
 import { fetchPopular } from "../../Api/deviantArt/devart";
 import { PopularDevArtData, PopularResult } from "../../Api/deviantArt/types";
+import { DevArtPageProps } from "../../containers/types";
 import { _openBrowserUrl } from "../../utils";
+import { PressableAnim } from "../animated/AnimPressable";
 import LoadingView from "../loadingView";
 import { ThemeColors } from "../types";
 
 type Props = {
     query: string;
+    navigation: NavigationProp<any>;
     colors: ThemeColors;
 }
-const DeviantArtImages = ({query, colors}:Props) => {
+const DeviantArtImages = ({query, navigation, colors}:Props) => {
     const [images, setImages] = useState<PopularDevArtData>();
     const [loading, setLoading] = useState<boolean>(true);
 
     const searchImages = async() => {
         // const filteredQuery = query.replace(/[\u00D7-]/g, ' x ');
         try {
-            const art = await fetchPopular(query);
+            const art = await fetchPopular(query, 'popular');
             return art;
         } catch (e) {
             console.log('Search:', e);
@@ -27,7 +32,7 @@ const DeviantArtImages = ({query, colors}:Props) => {
         
     }
 
-    const ArtView = ({item}:{item:PopularResult}) => {
+    const artView = ({item}:{item:PopularResult}) => {
         return(
             <Pressable onPress={() => _openBrowserUrl(item.url, colors.primary, colors.text)}>
                 <FastImage fallback source={{ uri: item.preview.src }} resizeMode='contain' style={{ width: 130, height: 190, marginHorizontal: 10 }} />
@@ -35,11 +40,19 @@ const DeviantArtImages = ({query, colors}:Props) => {
         );
     }
 
+    const FooterLoad = () => {
+        return(
+            <View style={{height:190, width:130, justifyContent:'center', alignItems:'center', }}>
+                <Pressable onPress={() => navigation.navigate('DeviantArt', {query:query, data:images})} style={{alignItems:'center', justifyContent:'center', borderWidth:1, borderRadius:12, borderColor:colors.primary, padding:20}}>
+                    <Text style={{color:colors.text}}>View More</Text>
+                </Pressable>
+            </View>
+        );
+    }
+
     useEffect(() => {
         let isMounted = true;
-        const queryTest = query.replace(/(Season \d+)|(\d+(nd|st|rd|th) Season)/gm, '');
-        console.log('Query:', queryTest);
-        if (!images) {
+        if (!images && isMounted) {
             searchImages().then(art => {
                 if (isMounted) {
                     setImages(art);
@@ -58,12 +71,19 @@ const DeviantArtImages = ({query, colors}:Props) => {
 
     return(
         <View>
-            <Text style={{ marginLeft: 10, marginTop: 20, fontSize: 28, fontWeight: 'bold', color: colors.text }}>Fan Art</Text>
+            <View style={{flexDirection:'row', alignItems:'center'}}>
+                <Text style={{ marginLeft: 10, marginTop: 20, fontSize: 28, fontWeight: 'bold', color: colors.text }}>Fan Art</Text>
+                <PressableAnim style={{ marginTop: 20, marginLeft: 10, borderRadius:8, borderColor:colors.primary, borderWidth:.8, padding:2, paddingHorizontal:10}} onPress={() => navigation.navigate('DeviantArt', {query:query, data:images})}>
+                    <Text style={{color:colors.primary, fontSize:12}}>View More</Text>
+                </PressableAnim>
+            </View>
             <FlatList
                 data={images?.results ?? []}
-                renderItem={ArtView}
+                renderItem={artView}
                 horizontal
                 keyExtractor={({deviationid}) => deviationid}
+                getItemLayout={(data, index) => ({length: 190, offset: 190 * index, index})}
+                ListFooterComponent={FooterLoad}
             />
         </View>
     );
