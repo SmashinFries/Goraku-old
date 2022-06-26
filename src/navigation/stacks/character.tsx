e16736888e7243c31f3f734092f2423fd725acbc
SmashinFries <smashinfries1@gmail.com>
2022-06-26 01:25:36 -0500

    added devart detail screen

--- src/navigation/stacks/character.tsx
+++ src/navigation/stacks/character.tsx
@@ -1,6 +1,7 @@
 import { createNativeStackNavigator } from "@react-navigation/native-stack";
 import React from "react";
 import { CharacterTab, CharDetailScreen } from "../../containers/character";
+import DeviantArtPage from "../../containers/deviantArt/devartPage";
 import { StaffInfo } from "../../containers/staff/staffPage";
 import { CharStackProps } from "../../containers/types";
 
@@ -13,6 +14,7 @@ const CharacterStack = ({navigation, route}: CharStackProps) => {
             <Stack.Screen name="Character" component={CharacterTab} initialParams={{ data:data }} options={{headerTransparent:true}} />
             <Stack.Screen name="CharDetail" component={CharDetailScreen} initialParams={{inStack: true}} />
             <Stack.Screen name="StaffDetail" component={StaffInfo} initialParams={{inStack: true}}/>
+            <Stack.Screen name="DeviantArtDetail" component={DeviantArtPage} initialParams={{inStack: false}}/>
         </Stack.Navigator>
     );
 }
