241ffdc2ca315aa731a78d87fb6f99e3c0b01c74
SmashinFries <smashinfries1@gmail.com>
2022-07-02 23:33:05 -0500

    update

--- src/navigation/stacks/character.tsx
+++ src/navigation/stacks/character.tsx
@@ -10,8 +10,8 @@ const Stack = createNativeStackNavigator();
 const CharacterStack = ({navigation, route}: CharStackProps) => {
     const { data } = route.params;
     return(
-        <Stack.Navigator initialRouteName="Character" screenOptions={{headerTransparent:false}} >
-            <Stack.Screen name="Character" component={CharacterTab} initialParams={{ data:data }} options={{headerTransparent:true}} />
+        <Stack.Navigator initialRouteName="Character" >
+            <Stack.Screen name="Character" component={CharacterTab} initialParams={{ data:data }} options={{title:'Characters'}}  />
             <Stack.Screen name="CharDetail" component={CharDetailScreen} initialParams={{inStack: true}} />
             <Stack.Screen name="StaffDetail" component={StaffInfo} initialParams={{inStack: true}}/>
             <Stack.Screen name="DeviantArtDetail" component={DeviantArtPage} initialParams={{inStack: false}}/>
